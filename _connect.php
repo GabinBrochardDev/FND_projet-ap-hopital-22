7767ce9b57c4f8cf7c94a33d02a8897831fa20b4
LudovicProuxDev <ludovic.proux@outlook.fr>
2022-11-15 14:05:33 +0100

    Ajout du lien à la page d'un nouveau membre personnel et creation de la procedure BDD'

---
+++ _connect.php
@@ -0,0 +1,32 @@
+<?php
+    // Connexion BDD Developpement
+
+    $servername = "172.16.193.254";
+    $username = "hopital-user";
+    $password = "bKmKTazcwWl3emRc";
+    $db = "hopital-data";
+
+    // Create connection
+    $connexion_db = new mysqli($servername, $username, $password, $db);
+
+    // Check connection
+    if ($connexion_db->connect_error) {
+        die("Connection failed: " . $connexion_db->connect_error);
+    }
+    
+
+    /* Connexion BDD WSL
+
+    $servername = "localhost";
+    $username = "root";
+    $password = '$SrvLudo+00!';
+    $db = "hopital-data";
+
+    // Create connection
+    $connexion_db = new mysqli($servername, $username, $password, $db);
+
+    // Check connection
+    if ($connexion_db->connect_error) {
+        die("Connection failed: " . $connexion_db->connect_error);
+    }*/
+?>
--- validation_personnel.php
+++ validation_personnel.php
@@ -1,36 +1,7 @@
 <?php
 
-/* Connexion BDD Developpement
-
-  $servername = "172.16.193.254";
-  $username = "hopital-user";
-  $password = "bKmKTazcwWl3emRc";
-  $db = "hopital-data";
-
-  // Create connection
-  $connexion_db = new mysqli($servername, $username, $password, $db);
-
-  // Check connection
-  if ($connexion_db->connect_error) {
-    die("Connection failed: " . $connexion_db->connect_error);
-  }
-*/
-
-//* Connexion BDD WSL
-
-  $servername = "localhost";
-  $username = "root";
-  $password = '$SrvLudo+00!';
-  $db = "hopital-data";
-
-  // Create connection
-  $connexion_db = new mysqli($servername, $username, $password, $db);
-
-  // Check connection
-  if ($connexion_db->connect_error) {
-    die("Connection failed: " . $connexion_db->connect_error);
-  }
-
+  // Informations de la connnexion à la base
+  require('_connect.php');
 
 ?>
 
